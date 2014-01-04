a0ae851879f06e47ffff7239d49597ba508e4fa2
NimbusBP1729 <patjz2@yahoo.com>
2014-01-04 09:53:05 -0500

    removed mockito dependency

--- core/src/main/java/com/projecthawkthorne/client/HawkthorneGame.java
+++ core/src/main/java/com/projecthawkthorne/client/HawkthorneGame.java
@@ -295,20 +295,14 @@ public class HawkthorneGame extends Game {
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
