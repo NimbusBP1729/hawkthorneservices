079ce55818c52e2c76ca254e97d5682780eebb6e
NimbusBP1729 <patjz2@yahoo.com>
2014-04-14 15:10:13 -0400

    switches from cocoatouch to apple

--- ios/src/main/java/com/projecthawkthorne/ios/HawkthorneGameApplicationDelegate.java
+++ ios/src/main/java/com/projecthawkthorne/ios/HawkthorneGameApplicationDelegate.java
@@ -1,7 +1,7 @@
 package com.projecthawkthorne.ios;
 
-import org.robovm.cocoatouch.foundation.NSAutoreleasePool;
-import org.robovm.cocoatouch.uikit.UIApplication;
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
@@ -19,6 +19,6 @@ public class HawkthorneGameApplicationDelegate extends IOSApplication.Delegate {
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, HawkthorneGameApplicationDelegate.class);
-        pool.drain();
+        pool.close();
     }
 }
